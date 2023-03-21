6337f9af086a2ccf5cd75437cbe21f0668887760
Alex Mellbye <82661985+Alex-Mellbye@users.noreply.github.com>
2023-03-21 14:29:01 +0100

    Update Basic functions_lists_and_decimals.py

--- Basic functions_lists_and_decimals.py
+++ Basic functions_lists_and_decimals.py
@@ -1,25 +1,3 @@
-###################### SAS Proc freq med 'where' statement ########################
-
-# Here I run a Python versjon of the SAS proc freq command with a where statement (in this case where col1 is missing).
-# I input the name of the dataset (v02001b), and the two relevant variables [pandas.series] ('avgjpers' and 'statusrollekid').
-
-def frek_ved_missing(df, col1, col2):
-
-    display(df.loc[df[col1].isnull()][col2].value_counts(dropna=False))
-
-frek_ved_missing(v02001b, 'avgjpers', 'statusrollekid')
-
-# If I do not wish to make a function I can also write it directly like so:
-
-display(df.loc[df['col1'].isnull()]['col2'].value_counts(dropna=False))
-
-# If I wish to have more conditions I can write it like this;
-
-display(df.loc[(df['col1'].isnull()) & (df['col2'].notna())]['col3'].value_counts(dropna=False))
-
-# In this code I am applying a 'mask' - i.e. Im not changing the data directly but creating a temporary copy on which I run the value_counts() command.
-# A filter if you like. 
-# And its all contained within the [ ] of the .loc function. 
 
 
 ############### Functions ####################
