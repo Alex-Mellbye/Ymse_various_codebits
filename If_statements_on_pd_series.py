b13c7e35e66921eefbb1d1cd8aa6c2c0236cc348
Alex Mellbye <82661985+Alex-Mellbye@users.noreply.github.com>
2023-08-03 10:28:47 +0200

    Update If_statements_on_pd_series.py

--- If_statements_on_pd_series.py
+++ If_statements_on_pd_series.py
@@ -25,15 +25,34 @@ df2['teller2'] = df2.apply(sak_rettskr_avgj, axis=1)
 print('Teller2')
 print(df2["teller2"].value_counts())
 
+
+
 # Under bruken vi en .loc() funksjon med flere betingelser. Dette er mer effektiv kode enn med .apply() funksjon, men tilgjengjeld 
 # så får vi kun et utfall. 
 
-df2.loc[(df2['sak_rettskr_avgj'] != '') | (df2['sak_rettskr_avgj'] != '0') | (df2['sak_rettskr_avgj'] != '0000'), 'tellerx'] = 1
-print('Tellerx')
-print(df2["tellerx"].value_counts())
+df2.loc[(df2['var1'] != '') | (df2['var1'] != '0') | (df2['var1'] != '0000'), 'var2'] = 1
+print('var2')
+print(df2["var2"].value_counts())
+
+
 
 # Her bruker vi .loc() funksjonen for en betingelse. Litt mer effektiv kode enn numpy.where men også bare et utfall.
 
-df2.loc[(df2['sak_rettskr_avgj'] == ''), 'teller_test'] = 1
-print('Teller_test')
-print(df2["teller_test"].value_counts())
+df2.loc[(df2['var1'] == ''), 'var2'] = 1
+print('var2')
+print(df2["var2"].value_counts())
+
+
+
+# Her bruker vi .loc() men med flere utfall
+
+df.loc[(df['var1'] == '000'), ['var2', 'var1']] = (1, pd.NA)
+print('var2')
+print(df2["var2"].value_counts())
+print("")
+print('var1')
+print(df2["var1"].value_counts())
+
+
+
+
